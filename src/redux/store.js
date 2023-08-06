de27f146460fc87d3e39670479fe7a6ceb42402b
anjali-singh8479 <shyanj05@gmail.com>
2023-08-06 15:47:19 +0530

    applying logger and redux-devtools-extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-
-// import cakeReducer from "./cakes/cakeReducer"
-
 import rootreducer from "./rootReducer";
-const {createStore}=require("redux")
-// const { default: cakeReducer } = require("./redux/cakeReducer")
-const store=createStore(rootreducer);
-console.log(store.getState);
+import {composeWithDevTools} from 'redux-devtools-extension'
+const {createStore,applyMiddleware}=require("redux")
+const {logger} = require("redux-logger");
+/* logger logs all the information related to store in a console
+logger shows the information like before transition state action and after transition state object as an output */
+
+/* now compositeDevTools is imported and it is now the second parameter to the store and it accepts
+apply middleware as it parameter. It is very useful while using redux especially for debugging */
+const store=createStore(rootreducer,composeWithDevTools(applyMiddleware(logger)));
 export default store
 
